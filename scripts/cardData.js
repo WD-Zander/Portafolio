5bcdcb8b7608fd55fd382021685f32223d14005f
Zander J <zanderjosue05@gmail.com>
2025-01-31 08:02:59 -0400

    .

--- scripts/cardData.js
+++ scripts/cardData.js
@@ -4,29 +4,28 @@ export const cardData = [
         card: {
             link: "https://images.pexels.com/photos/30434990/pexels-photo-30434990/free-photo-of-retrato-de-un-perro-peludo-contra-el-cielo-azul.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
             title: "Proyecto 1",
-            info: "Descripción del Proyecto 1"
+            info: "DSimón José Antonio de la Santísima Trinidad Bolívar Ponte y Palacios Blanco (Caracas, Imperio español; 24 de julio de 1783nota 4​nota 5​-Santa Marta, Gran Colombia; 17 de diciembre de 1830) también conocido como el Libertador, fue un militar, estratega y político venezolano."
         }
     },
     {
         card: {
             link: "https://images.pexels.com/photos/30434990/pexels-photo-30434990/free-photo-of-retrato-de-un-perro-peludo-contra-el-cielo-azul.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
             title: "Proyecto 2",
-            info: "Descripción del Proyecto 2"
+            info: "Simón José Antonio de la Santísima Trinidad Bolívar Ponte y Palacios Blanco (Caracas, Imperio español; 24 de julio de 1783nota 4​nota 5​-Santa Marta, Gran Colombia; 17 de diciembre de 1830) también conocido como el Libertador, fue un militar, estratega y político venezolano.2"
         }
     },
     {
         card: {
             link: "https://images.pexels.com/photos/30434990/pexels-photo-30434990/free-photo-of-retrato-de-un-perro-peludo-contra-el-cielo-azul.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
             title: "Proyecto 3",
-            info: "Descripción del Proyecto 3"
+            info: "DSimón José Antonio de la Santísima Trinidad Bolívar Ponte y Palacios Blanco (Caracas, Imperio español; 24 de julio de 1783nota 4​nota 5​-Santa Marta, Gran Colombia; 17 de diciembre de 1830) también conocido como el Libertador, fue un militar, estratega y político venezolano."
         }
     },
     {
         card: {
             link: "https://images.pexels.com/photos/30434990/pexels-photo-30434990/free-photo-of-retrato-de-un-perro-peludo-contra-el-cielo-azul.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
             title: "Proyecto 3",
-            info: "Descripción del Proyecto 3"
+            info: "DSimón José Antonio de la Santísima Trinidad Bolívar Ponte y Palacios Blanco (Caracas, Imperio español; 24 de julio de 1783nota 4​nota 5​-Santa Marta, Gran Colombia; 17 de diciembre de 1830) también conocido como el Libertador, fue un militar, estratega y político venezolano."
         }
     }
-    
 ];
